073ac589d95570342075c3e267864eb48a3e4290
kltzqu <kltzqu@gmail.com>
2026-02-14 23:15:56 +0200

    updated frontend design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,17 +11,18 @@ function App() {
         if (!res.ok) {
           throw new Error('Network response was not ok');
         }
-        return res.text();  // Здесь должно быть .text(), чтобы получить текст
+        return res.text();
       })
       .then(text => setData(text))
       .catch(err => setError(err.message));
   }, []);
 
   return (
-    <div>
-      <h1>From backend:</h1>
-      {error ? <p>Error: {error}</p> : <p>{data}</p>}
-    </div>
+    <body>
+      <div className="page">
+        <div className="glassText">login window</div>
+      </div>
+    </body>
   );
 }
 
