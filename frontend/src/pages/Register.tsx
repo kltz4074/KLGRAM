4d807f6de04afd7134e47e03fc87a78ca1690a44
kltzqu <kltz4074@gmail.com>
2026-02-17 12:28:32 +0200

    register page but page not corrects in correct position after openning register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
+import "./Register.css"
+
 function Register() {
     return (
         <div className="wrapper">
-            <h1>Home</h1>
+          <div className="page">
+            <div className="title">KLGRAM</div>
+
+            <div className="container">
+              <input className="input" placeholder="Username" />
+              <input className="input" placeholder="Email" />
+              <input className="input" placeholder="Password" type="password" />
+              <input className="input" placeholder="confirm Password" type="password" />
+              
+              <button className="Button">Register</button>
+              <p className='miniText'>Already have an account? <Link to="/">Log in</Link></p>
+            </div>
+          </div>
         </div>
+
     );
 }
 
