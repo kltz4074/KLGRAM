35df81c193d12066a752a5395fbffd87c3dfde43
kltzqu <kltzqu@gmail.com>
2026-02-19 23:21:55 +0200

    fixed backend sending to repository

---
+++ Backend/src/main/java/com/klgram/klgram/KlgramApplication.java
@@ -0,0 +1,13 @@
+package com.klgram.klgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlgramApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/klgram/klgram/KlgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klgram.klgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
